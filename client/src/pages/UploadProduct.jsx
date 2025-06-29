ef12717cb9425f7d6c34659a98f39def38c0a797
Chand <chandraarnab92@gmail.com>
2025-06-29 16:05:42 +0530

    Fix: Corrected SuccessAlert import and updated alert

--- client/src/pages/UploadProduct.jsx
+++ client/src/pages/UploadProduct.jsx
@@ -10,7 +10,7 @@ import AddFieldComponent from '../components/AddFieldComponent';
 import Axios from '../utils/Axios';
 import SummaryApi from '../common/SummaryApi';
 import AxiosToastError from '../utils/AxiosToastError';
-import successAlert from '../utils/successAlert';
+import successAlert from '../utils/SuccessAlert.js';
 
 const UploadProduct = () => {
   const [data, setData] = useState({
